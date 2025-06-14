9f8411144d6d23abdc5b503ca7b97ae9e4105a23
Selix2002 <smunmuno@umag.cl>
2025-06-14 00:13:10 -0400

    aber2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // 1) Carga las variables de entorno (incluye VITE_API_URL)
-  const env = loadEnv(mode, process.cwd(), '')
 
   // 2) Extrae la URL de tu API (por ejemplo "http://localhost:8000" o tu URL en Render)
   const API_URL = "https://proyectodb-cepas-v14l.onrender.com"
