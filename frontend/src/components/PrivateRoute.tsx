636a4232e73c5167f5152b55e249ade8b77f3f61
Sebastián Muñoz <smunmuno@umag.cl>
2025-07-30 13:35:35 -0300

    Cambios en seguridad

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,16 +1,12 @@
 // src/components/PrivateRoute.tsx
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../stores/AuthContext'
+import { useAuth } from '../stores/AuthContext';
 
 const PrivateRoute: React.FC = () => {
-  const { user, token } = useAuth()
+  const { token } = useAuth();
   if (!token) {
     return <Navigate to="/login" replace />;
-  }
-  if (user?.isAdmin == false) {
-    // Si el usuario no es admin, redirige a la página de inicio
-    return <Navigate to="/home" replace />;
   }
   // Si hay token, renderiza las rutas hijas
   return <Outlet />;
