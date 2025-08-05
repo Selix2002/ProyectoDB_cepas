08453d4524ae74536bbeb22c0c1a49ea8adb0dbe
Sebastián Muñoz <smunmuno@umag.cl>
2025-08-05 20:39:13 -0300

    XD

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,8 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     debug: bool = False  # default to False
     database_url: str
-    secret_key: str = "soyyolameafichaqliayquepasa"
-
+    secret_key: SecretStr = SecretStr("secret123")
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
