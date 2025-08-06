666a69f556f7b1abf58be7cc30ed65c16ceccb6a
Sebastián Muñoz <smunmuno@umag.cl>
2025-08-05 21:16:41 -0300

    ASDAS

--- frontend/src/components/CepasTable.tsx
+++ frontend/src/components/CepasTable.tsx
@@ -243,7 +243,7 @@ export default function CepasTable({
             pagination
             paginationPageSize={20}
             paginationPageSizeSelector={[20, 50, 70, 100]}
-            domLayout="autoHeight"
+            domLayout="normal"
           />
         </div>
       </div>
